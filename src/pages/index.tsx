ca88565d1c63ce6b258b867482813a8f9f10ad89
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2023-03-26 17:10:28 +0100

    fix folder hierachy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
-import { pageStyles, headingStyles, headingAccentStyles } from "./pageStyles"
+import { pageStyles, headingStyles, headingAccentStyles } from "../styles/pageStyles"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
