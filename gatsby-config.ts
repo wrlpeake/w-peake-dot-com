82836e9409934c1fac1512df02a8fd2261637fd5
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2023-03-26 17:15:42 +0100

    fix image import

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,10 +11,6 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
     __key: "images"
   }]
 };
