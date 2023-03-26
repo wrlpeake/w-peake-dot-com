f85ed9c9a335b497bdefb0a9613c9d798c79531f
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2023-03-26 17:06:06 +0100

    remove gatsby placeholder text

---
+++ src/pages/pageStyles.tsx
@@ -0,0 +1,56 @@
+export const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
+export const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+};
+export const headingAccentStyles = {
+  color: "#663399",
+};
+const paragraphStyles = {
+  marginBottom: 48,
+};
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+};
+const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0,
+};
+const doclistStyles = {
+  paddingLeft: 0,
+};
+const listItemStyles = {
+  fontWeight: 300,
+  fontSize: 24,
+  maxWidth: 560,
+  marginBottom: 30,
+};
+const linkStyle = {
+  color: "#8954A8",
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%",
+};
+const docLinkStyle = {
+  ...linkStyle,
+  listStyleType: "none",
+  display: `inline-block`,
+  marginBottom: 24,
+  marginRight: 12,
+};
+const descriptionStyle = {
+  color: "#232129",
+  fontSize: 14,
+  marginTop: 10,
+  marginBottom: 0,
+  lineHeight: 1.25,
+};
