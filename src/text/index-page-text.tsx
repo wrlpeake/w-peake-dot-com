73171817cede1d0fd264c89edf8da9ff672a8519
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2025-03-18 12:33:27 +0000

    implement-title-text-reveal-motion (#40)

---
+++ src/text/index-page-text.tsx
@@ -0,0 +1,14 @@
+export const title = "William Peake";
+export const description = "Software Engineer based in London, UK.";
+export const greeting = "— Hi, I'm Will ";
+export const technical =
+  "I have professional experience working with Java, Typescript & React, among other languages and frameworks," +
+  " as well as infrastructure tools such as AWS, Kubernetes, Docker and Kafka. I am currently working for a" +
+  " US-headquarted tech consultancy, 8th Light Inc, as a Senior Software Engineer and Consultant.";
+export const history =
+  "I found software development after a decade in sales and business development, predominantly in media," +
+  " spending the vast majority of my time trying to understand client problems and design solutions to fit" +
+  " their needs. I apply this same thought process to software engineering, working with colleagues and" +
+  " clients to build high-quality software products that are intuitive and maintainable.";
+export const experience =
+  "Previous experience: Ticketmaster, Digital Partners (a Munich Re company), Financial Times, 8th Light.";
