6d514ae174e975fe91345bbb9135f29d318587e5
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2025-03-18 10:20:51 +0000

    update 404 page (#39)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,26 @@
-import * as React from "react";
+import React from "react";
 import { Link, HeadFC, PageProps } from "gatsby";
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
+import { pageStyles, headingStyles, paragraphStyles } from "../styles/styles";
+import favicon from "../images/favicon.png";
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+      <p style={paragraphStyles}>Sorry, I couldn’t find what you were looking for.</p>
+      <p style={paragraphStyles}><Link to="/">Go Home</Link></p>
     </main>
   );
 };
 
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head: HeadFC = () => (
+  <head>
+    <title>William Peake | Page not found</title>
+    <link rel="icon" type="image/x-icon" href={favicon}></link>
+    <meta charSet="UTF-8" />
+    <meta name="description" content="Page not found" />
+    <meta name="author" content="William Peake" />
+  </head>
+);
